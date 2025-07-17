e1af2fcc99a207a2e11a21001d148db87d5adea9
Sergey Balalaev <sof.bix@mail.ru>
2025-07-17 23:18:09 +0400

    EmptyFile update

---
+++ Sources/Localinter/EmptyFile.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyFile.swift
+//  
+//
+//  Created by Sergey Balalaev on 19.03.2024.
+//
+
+// This empty file needs for fix the error with init library contented only Plugin
--- Sources/Localinter/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Sergey Balalaev on 19.03.2024.
-//
-
-import Foundation
